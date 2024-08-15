da0d27f967e962ffe7a99af655df78c8c9293664
Kenneth Bruce <me@kenneth-bruce.com>
2024-08-14 22:03:54 -0400

    start breaking out the init.lua

--- lua/custom/plugins/keymaps.lua
+++
@@ -1,3 +0,0 @@
-
-
-vim.keymap.set('n', '<leader>bn', ':bNext<CR>', { desc = 'Change to the next buffer' })
---
+++ lua/kennebru/plugins/remap.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<leader>vf', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>bn', ':bNext<CR>', { desc = 'Change to the next buffer' })
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Window mode nave
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
