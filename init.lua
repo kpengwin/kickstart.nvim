ad8d67e5e78c362dae5a6c566dda4c818e346024
Kenneth Bruce <kennebru@amazon.com>
2024-10-02 15:15:17 +0000

    dev desk changes

--- init.lua
+++ init.lua
@@ -538,7 +538,17 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        pylsp = {
+          settings =  {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  maxLineLength = 119
+                }
+              }
+            }
+          }
+        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -606,7 +616,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, python = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -744,13 +754,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'pappasam/papercolor-theme-slim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'PaperColorSlim'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
