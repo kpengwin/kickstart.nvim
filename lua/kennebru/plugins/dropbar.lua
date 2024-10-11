2077223c88f8d0a8b6df9b0feded4b34f200d10d
Kenneth Bruce <me@kenneth-bruce.com>
2024-10-11 09:50:10 -0400

    add leader p binding for dropbar

--- lua/kennebru/plugins/dropbar.lua
+++ lua/kennebru/plugins/dropbar.lua
@@ -3,4 +3,11 @@ return {
   dependencies = {
     'nvim-telescope/telescope-fzf-native.nvim',
   },
+  keys = function(_, keys)
+    local api = require 'dropbar.api'
+    return {
+      { '<leader>p', api.pick, desc = 'open dropbar [p]icker' },
+      unpack(keys),
+    }
+  end,
 }
